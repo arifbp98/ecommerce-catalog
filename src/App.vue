1e4f8d9f01293c0f2f24cb0ab8dfa2198781973a
arifbp98 <arifbudianto62@yahoo.co.id>
2023-10-29 21:57:16 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ProductDisplay />
+  </div>
+</template>
+
+<script>
+import './assets/style/style.css';
+import ProductDisplay from './components/ProductDisplay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay,
+  },
+};
+</script>
